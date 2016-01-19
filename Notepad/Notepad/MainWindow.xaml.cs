b25d5b64b36739b8a5ca996883ca2f3e59409f5f
Jun Kang <jkang027@yahoo.com>
2016-01-19 11:43:11 -0800

    Works with min. req. To add more

---
+++ Notepad/Notepad/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace Notepad
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void menuNew_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow menuNew = new MainWindow();
+            menuNew.Show();
+        }
+
+        private void menuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog menuOpen = new OpenFileDialog();
+            menuOpen.Filter = "Text files (recommended) (*.txt)|*.txt| All files (*.*)|*.*";
+            if (menuOpen.ShowDialog() == true)
+                textBox.Text = File.ReadAllText(menuOpen.FileName);
+        }
+
+        private void menuSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog menuSave = new SaveFileDialog();
+            menuSave.Filter = "Text file (*.txt)|*.txt";
+            if (menuSave.ShowDialog() == true)
+                File.WriteAllText(menuSave.FileName, textBox.Text);
+        }
+    }
+}
